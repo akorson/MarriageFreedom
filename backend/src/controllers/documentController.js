514ff2c68607f6880b82716193ada1563dbf57b0
akorson <96851223+akorson@users.noreply.github.com>
2023-04-08 04:54:50 -0500

    update

---
+++ backend/src/controllers/documentController.js
@@ -0,0 +1,54 @@
+const Document = require('../models/documentModel');
+const { generateDocument, mergeDocuments } = require('../utils/fileHandler');
+
+exports.createDocument = async (req, res) => {
+  try {
+    const documentData = req.body;
+    const userId = req.user._id;
+
+    const generatedFiles = await generateDocument(documentData, userId);
+    const mergedFile = await mergeDocuments(generatedFiles);
+
+    const newDocument = new Document({
+      user: userId,
+      title: documentData.title,
+      caseType: documentData.caseType,
+      file: mergedFile,
+      instructions: generatedFiles.instructions,
+    });
+
+    const savedDocument = await newDocument.save();
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        document: savedDocument,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+};
+
+exports.getDocumentsByUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const documents = await Document.find({ user: userId });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        documents,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+};
---
+++ backend/src/models/documentModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    instructions: {
+        type: String,
+        required: true,
+    },
+    caseType: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Document', documentSchema);
