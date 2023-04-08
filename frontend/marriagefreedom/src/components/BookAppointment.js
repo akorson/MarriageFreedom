ea7c676624ae6d680d49746ef924a9ca61cb2e0b
akorson <96851223+akorson@users.noreply.github.com>
2023-04-08 02:01:22 -0500

    Add CircleCI configuration

---
+++ frontend/marriagefreedom/src/components/BookAppointment.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+function BookAppointment() {
+  return (
+    <div>
+      <Helmet>
+        <title>Book an Appointment - Illinois Family Law & Divorce Attorneys</title>
+        <meta name="description" content="Schedule a $199 consultation with experienced Illinois family law and divorce attorneys for adoptions, child custody, child support, guardianship, order of protection, prenuptial agreements, postnuptial agreements, enforcement of judgments, alimony, spousal support, and legal separation cases." />
+        <meta name="keywords" content="Illinois, adoptions, family lawyers, divorces, divorce attorneys, child custody, child support, guardianship, order of protection, prenuptial agreements, postnuptial agreements, enforcement of judgments, alimony, spousal support, legal separation" />
+      </Helmet>
+      <h1>Book an Appointment</h1>
+      <p>
+        Schedule a 45-minute consultation with an attorney for $199. Our
+        experienced attorneys can assist you with adoptions, child custody,
+        child support, guardianship, order of protection, prenuptial agreements,
+        postnuptial agreements, enforcement of judgments, alimony, spousal
+        support, and legal separation cases.
+      </p>
+      <div style={{ width: '100%', height: '800px' }}>
+        <iframe
+          src="https://calendly.com/chiattorney/attorney-consultation"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          title="Book Appointment"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default BookAppointment;
---
+++ frontend/marriagefreedom/src/components/DocumentReview.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DocumentReview() {
+  const [documentContent, setDocumentContent] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDocumentContent = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await axios.get('<your-api-fetch-endpoint>');
+        setDocumentContent(response.data.content);
+      } catch (error) {
+        setError('An error occurred while fetching the document content.');
+      }
+
+      setLoading(false);
+    };
+
+    fetchDocumentContent();
+  }, []);
+
+  const handleSaveChanges = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      await axios.post('<your-api-save-endpoint>', {
+        content: documentContent,
+      });
+    } catch (error) {
+      setError('An error occurred while saving the document content.');
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <h1>Document Review</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          <textarea
+            value={documentContent}
+            onChange={(e) => setDocumentContent(e.target.value)}
+            rows={20}
+            cols={80}
+          ></textarea>
+          <button onClick={handleSaveChanges}>Save Changes</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default DocumentReview;
